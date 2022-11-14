b46dd8aae7815a37522339064556aa5bdd6e1531
iiiLearningAccount <98540432+iiiLearningAccount@users.noreply.github.com>
2022-11-14 19:26:49 +0100

    Veroeffentlichung

--- Dokumentation/LaTex_Projekt/Listings/VorgangController.cs
+++ Dokumentation/LaTex_Projekt/Listings/VorgangController.cs
@@ -1,74 +1,147 @@
 ﻿using Kendo.Mvc.Extensions;
 using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ReklaTool.Models;
 using ReklaTool.Services;
 
 namespace ReklaTool.Controllers
-{
+{    
+    [Authorize]
     public class VorgangController : Controller
     {
         private readonly IEndpointService _uiEndpointService;
         public VorgangController(IEndpointService uiEndpointService)
         {
             _uiEndpointService = uiEndpointService;
-        }
-
-        public async Task<ActionResult> VorgangAuswahl_Read([DataSourceRequest] DataSourceRequest request, FilterRequest model)
+        }            
+		public async Task<ActionResult> VorgangAuswahl_Read([DataSourceRequest] DataSourceRequest request, FilterRequest model)
         {
-            var vorgangInfos = _uiEndpointService.GetVorgangInfos(model);
-            var result = Json(await vorgangInfos.ToDataSourceResultAsync(request));
-            return result;
+            try
+            {
+                var vorgangInfos = _uiEndpointService.GetVorgangInfos(model);
+                var result = Json(await vorgangInfos.ToDataSourceResultAsync(request));
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return BadRequest();                
+            }
         }
         public async Task<ActionResult> Fahrzeugdaten_Read([DataSourceRequest] DataSourceRequest request, FilterRequest model)
         {
-            var outputObject = _uiEndpointService.GetFahrzeugDaten(model);
-            var result = Json(await outputObject.ToDataSourceResultAsync(request));
-            return result;
+            try
+            {
+                var outputObject = _uiEndpointService.GetFahrzeugDaten(model);
+                var result = Json(await outputObject.ToDataSourceResultAsync(request));
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return BadRequest();
+            }
         }
         public async Task<ActionResult> Kosten_Read([DataSourceRequest] DataSourceRequest request, FilterRequest model)
         {
-            var outputObject = _uiEndpointService.GetKosten(model);
-            var result = Json(await outputObject.ToDataSourceResultAsync(request));
-            return result;
+            try
+            {
+                var outputObject = _uiEndpointService.GetKosten(model);
+                var result = Json(await outputObject.ToDataSourceResultAsync(request));
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return BadRequest();
+            }
         }
         public async Task<ActionResult> Ausstattung_Read([DataSourceRequest] DataSourceRequest request, FilterRequest model)
         {
-            var outputObject = _uiEndpointService.GetAusstattung(model);
-            var result = Json(await outputObject.ToDataSourceResultAsync(request));
-            return result;
+            try
+            {
+                var outputObject = _uiEndpointService.GetAusstattung(model);
+                var result = Json(await outputObject.ToDataSourceResultAsync(request));
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return BadRequest();
+            }
         }
         public async Task<ActionResult> Positionen_Read([DataSourceRequest] DataSourceRequest request, FilterRequest model)
         {
-            var outputObject = _uiEndpointService.GetPositionen(model);
-            var result = Json(await outputObject.ToDataSourceResultAsync(request));
-            return result;
+            try
+            {
+                var outputObject = _uiEndpointService.GetPositionen(model);
+                var result = Json(await outputObject.ToDataSourceResultAsync(request));
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return BadRequest();
+            }
         }
         public async Task<ActionResult> Regeln_Read([DataSourceRequest] DataSourceRequest request, FilterRequest model)
         {
-            var outputObject = _uiEndpointService.GetRegeln(model);
-            var result = Json(await outputObject.ToDataSourceResultAsync(request));
-            return result;
+            try
+            {
+                var outputObject = _uiEndpointService.GetRegeln(model);
+                var result = Json(await outputObject.ToDataSourceResultAsync(request));
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return BadRequest();
+            }
         }
         [HttpGet]
         public FileResult Pdf_Read(FilterRequest model)
         {
-            var byteData = _uiEndpointService.GetPdf(model);
-            var output = File(byteData, "application/pdf");
-            return output;
+            try
+            {
+                var byteData = _uiEndpointService.GetPdf(model);
+                var output = File(byteData, "application/pdf");
+                return output;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return ;
+            }
         }
 
         public FileContentResult Xml_Read(string aktenzeichen_typ, string aktenzeichen, bool isSchnellsuche, int vorgangIndex)
         {
-            FilterRequest model = new FilterRequest()
+            try
             {
-                Aktenzeichen_Typ = aktenzeichen_typ,
-                Aktenzeichen = aktenzeichen,
-                IsSchnellsuche = isSchnellsuche,
-                VorgangIndex = vorgangIndex
-            };
-            byte[] byteData = _uiEndpointService.GetXml(model);
-            return File(byteData, "text/xml", "Kalkulation.xml");
-        }
+                FilterRequest model = new FilterRequest()
+                {
+                    Aktenzeichen_Typ = aktenzeichen_typ,
+                    Aktenzeichen = aktenzeichen,
+                    IsSchnellsuche = isSchnellsuche,
+                    VorgangIndex = vorgangIndex
+                };
+                byte[] byteData = _uiEndpointService.GetXml(model);
+                return File(byteData, "text/xml", "Kalkulation.xml");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                return BadRequest();
+            }
+        }      
     }
 }
--- Dokumentation/LaTex_Projekt/Projektdokumentation.lol
+++ Dokumentation/LaTex_Projekt/Projektdokumentation.lol
@@ -1,2 +1,3 @@
-\contentsline {lstlisting}{\numberline {1}ResponseModel.cs -- XML-Annotationen}{xi}{lstlisting.135}%
-\contentsline {lstlisting}{\numberline {2}View-Komponenten -- Checkbox und Auswahlliste}{xii}{lstlisting.170}%
+\contentsline {lstlisting}{\numberline {1}ResponseModel.cs -- XML-Annotationen}{xi}{lstlisting.139}%
+\contentsline {lstlisting}{\numberline {2}VorgangController.cs -- Exception Handling}{xii}{lstlisting.172}%
+\contentsline {lstlisting}{\numberline {3}View-Komponenten -- Checkbox und Auswahlliste}{xiii}{lstlisting.216}%
